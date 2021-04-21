aa4c4ddf48177ec3b06ff90df3c49a91e24fc05d
David Goodell <dgoodell@rhythmictech.com>
2021-04-21 11:09:27 -0400

    add the IAM role used for backups to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "efs_file_system_id" {
   description = "EFS File System ID"
   value       = try(aws_efs_file_system.this[0].id, "")
 }
+
+output "efs_backup_iam_role" {
+  description = "ARN of the IAM Role created to run AWS Backup"
+  value       = try(aws_iam_role.backup[0].id, "")
+}
